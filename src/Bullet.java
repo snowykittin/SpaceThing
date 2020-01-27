2be54a5a44c7ded8b629f39204792ed263b428bd
snowykittin <sextosar000@warren.k12.in.us>
2020-01-27 12:56:33 -0500

    Initial commit

---
+++ src/Bullet.java
@@ -0,0 +1,41 @@
+import org.w3c.dom.css.Rect;
+
+import java.awt.*;
+
+public class Bullet {
+
+    int x, y, diameter = 10;
+    int speed, dx, dy = 10;
+    boolean remove;
+
+    public Bullet(Player player){
+
+        x = player.getX() + player.getWIDTH()/2 - diameter/2;
+        y = player.getY() - diameter;
+    }
+
+    public void move(){
+        y-=dy;
+    }
+
+    public Rectangle getBounds(){
+        return new Rectangle(x, y, diameter, diameter);
+    }
+
+    public void paint(Graphics g){
+        g.setColor(Color.white);
+        g.fillOval(x, y, diameter, diameter);
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public void setRemove(){
+        remove = true;
+    }
+    public boolean getRemove(){
+        return remove;
+    }
+
+}
