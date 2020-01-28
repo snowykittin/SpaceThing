1a05b98e7568984ccc275b65495067e9c10c54a0
snowykittin <sextosar000@warren.k12.in.us>
2020-01-28 12:42:01 -0500

    fixing

--- src/Board.java
+++ src/Board.java
@@ -37,12 +37,28 @@ public class Board extends JPanel implements ActionListener {
     }
 
     public void checkCollisions(){
-
+        for(int i = bullets.size()-1;  i>= 0; i--){
+            for(int j = 0; j < enemies.length; j++){
+                for(int k = 0; k < enemies[0].length; k++){
+                    if(enemies[j][k] != null){
+                        if(bullets.get(i).getBounds().intersects(enemies[j][k].getBounds())){
+                            bullets.get(i).getRemove();
+                            enemies[j][k] = null;
+                            break;
+                        }
+                    }
+                }
+            }
+            if(bullets.get(i).getRemove() == true){
+                bullets.remove(bullets.get(i));
+            }
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         long currentTime = System.currentTimeMillis();
+        checkCollisions();
         if(game.isSpacePressed() && currentTime - bulletDelay >= 250){
             bullets.add(new Bullet(player));
             bulletDelay = System.currentTimeMillis();
@@ -61,7 +77,9 @@ public class Board extends JPanel implements ActionListener {
         if(currentTime - timeDelay >= 1000){
             for(int row = 0; row < 5; row++){
                 for(int col = 0; col < 10; col++){
-                    enemies[row][col].move();
+                    if(enemies[row][col] != null){
+                        enemies[row][col].move();
+                    }
                 }
             }
             timeDelay = System.currentTimeMillis();
@@ -85,7 +103,9 @@ public class Board extends JPanel implements ActionListener {
         player.paint(g);
         for(int row = 0; row < 5; row++){
             for(int col = 0; col < 10; col++){
-                enemies[row][col].paint(g);
+                if(enemies[row][col] != null){
+                    enemies[row][col].paint(g);
+                }
             }
         }
         for(Bullet bullet: bullets){
